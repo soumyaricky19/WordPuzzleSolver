8294bac2b9307d9a93c53c2ca4cb79ce93191ca6
Soumya Mishra <soumya.ricky19@gmail.com>
2016-10-20 20:40:12 -0500

    Modular

--- WordPuzzleSolver.java
+++ WordPuzzleSolver.java
@@ -5,8 +5,12 @@ import java.io.*;
 
 public class WordPuzzleSolver 
 {
-	static int rows,columns,diagonal;
+	static int rows=0,columns=0,directions=8;
 	static int counter=0,count=0;
+	char a[][]= new char[rows][columns];
+	static LinkedList<String> l1= new LinkedList<>();
+	static BinarySearchTree<String> l2= new BinarySearchTree<>();
+	static QuadraticProbingHashTable<String> l3=new QuadraticProbingHashTable<String>();
 	public static void main (String args[])
 	{		
 		long start, end;
@@ -15,9 +19,7 @@ public class WordPuzzleSolver
 		try 
 		{
 			System.out.println("Loading dictionary. Please wait ...");
-			LinkedList<String> l1= new LinkedList<>();
-			BinarySearchTree<String> l2= new BinarySearchTree<>();
-			QuadraticProbingHashTable<String> l3=new QuadraticProbingHashTable<String>();
+
 			String word=null;
 			//Read dictionary words
 			BufferedReader f = new BufferedReader(new FileReader("dictionary.txt"));
@@ -41,238 +43,50 @@ public class WordPuzzleSolver
 			//Insert random letters
 			for(int i=0;i<rows;i++)
 				for (int j=0;j<columns;j++)
-				{
 					a[i][j]= (char)(97+(java.lang.Math.random()*100)%26);
-				}
+			
 			
 			displayCrossword(a);
+			
 // Search from Linked list		
             start = System.currentTimeMillis( );
-            for(int i=0;i<rows;i++)
-				for (int j=0;j<columns;j++)
-				{
-					String str=String.valueOf(a[i][j]);
-					searchLinkedList(str,l1);
-					//Go right
-					
-					for (int y=j+1; y<columns ;y++)
-					{
-						str+=String.valueOf(a[i][y]);
-						searchLinkedList(str,l1);
-					}
-						
-					//Go left
-					str=String.valueOf(a[i][j]);
-					for (int y=j-1; y>=0 ;y--)
-					{
-						str+=String.valueOf(a[i][y]);
-						searchLinkedList(str,l1);
-					}
-						
-					//Go down
-					str=String.valueOf(a[i][j]);
-					for (int x=i+1; x<rows ;x++)
-					{
-						str+=String.valueOf(a[x][j]);
-						searchLinkedList(str,l1);
-					}	
-						
-					//Go up
-					str=String.valueOf(a[i][j]);
-					for (int x=i-1; x>=0 ;x--)
-					{
-						str+=String.valueOf(a[x][j]);
-						searchLinkedList(str,l1);
-					}	
-						
-					//Go SE	
-					str=String.valueOf(a[i][j]);
-					for (int x=i+1,y=j+1; x<rows && y<columns ;x++,y++)
-					{
-						str+=String.valueOf(a[x][y]);
-						searchLinkedList(str,l1);
-					}	
-						
-					//Go SW
-					str=String.valueOf(a[i][j]);
-					for (int x=i+1,y=j-1; x<rows && y>=0 ;x++,y--)
-					{
-						str+=String.valueOf(a[x][y]);
-						searchLinkedList(str,l1);
-					}
-					//Go NW
-					str=String.valueOf(a[i][j]);
-					for (int x=i-1,y=j-1; x>=0 && y>=0 ;x--,y--)
-					{
-						str+=String.valueOf(a[x][y]);
-						searchLinkedList(str,l1);
-					}
-					
-					//Go NE
-					str=String.valueOf(a[i][j]);
-					for (int x=i-1,y=j+1; x>=0 && y<columns ;x--,y++)
-					{
-						str+=String.valueOf(a[x][y]);
-						searchLinkedList(str,l1);
-					}			
-
-				}
-	System.out.println("Counter"+counter);
-	counter=0;
-	end = System.currentTimeMillis( );
-    System.out.println( "For Linked list, Elapsed time in ms: " + (end-start) );
+            counter=0;
+            /*
+            for(int i=0;i<a.length;i++)
+				for (int j=0;j<a[i].length;j++)
+					for (int k=0;k<directions;k++)
+						searchLinkedList(goDirections(a,i,j,k),l1);						
+			*/	
+            System.out.println("Counter"+counter);
+            end = System.currentTimeMillis( );
+            System.out.println( "For Linked list, Elapsed time in ms: " + (end-start) );
 	
 
 // Search from Trees
-    start = System.currentTimeMillis( );
-    for(int i=0;i<rows;i++)
-		for (int j=0;j<columns;j++)
-		{
-			String str=String.valueOf(a[i][j]);
-			searchTree(str,l2);
-			//Go right
-			
-			for (int y=j+1; y<columns ;y++)
-			{
-				str+=String.valueOf(a[i][y]);
-				searchTree(str,l2);
-			}
-				
-			//Go left
-			str=String.valueOf(a[i][j]);
-			for (int y=j-1; y>=0 ;y--)
-			{
-				str+=String.valueOf(a[i][y]);
-				searchTree(str,l2);
-			}
-				
-			//Go down
-			str=String.valueOf(a[i][j]);
-			for (int x=i+1; x<rows ;x++)
-			{
-				str+=String.valueOf(a[x][j]);
-				searchTree(str,l2);
-			}	
-				
-			//Go up
-			str=String.valueOf(a[i][j]);
-			for (int x=i-1; x>=0 ;x--)
-			{
-				str+=String.valueOf(a[x][j]);
-				searchTree(str,l2);
-			}	
-				
-			//Go SE	
-			str=String.valueOf(a[i][j]);
-			for (int x=i+1,y=j+1; x<rows && y<columns ;x++,y++)
-			{
-				str+=String.valueOf(a[x][y]);
-				searchTree(str,l2);
-			}	
-				
-			//Go SW
-			str=String.valueOf(a[i][j]);
-			for (int x=i+1,y=j-1; x<rows && y>=0 ;x++,y--)
-			{
-				str+=String.valueOf(a[x][y]);
-				searchTree(str,l2);
-			}
-			//Go NW
-			str=String.valueOf(a[i][j]);
-			for (int x=i-1,y=j-1; x>=0 && y>=0 ;x--,y--)
-			{
-				str+=String.valueOf(a[x][y]);
-				searchTree(str,l2);
-			}
-			
-			//Go NE
-			str=String.valueOf(a[i][j]);
-			for (int x=i-1,y=j+1; x>=0 && y<columns ;x--,y++)
-			{
-				str+=String.valueOf(a[x][y]);
-				searchTree(str,l2);
-			}			
-
-		}
-System.out.println("Counter"+counter);
-end = System.currentTimeMillis( );
-System.out.println( "For tree, Elapsed time in ms: " + (end-start) );
+            start = System.currentTimeMillis( );
+            counter=0;
+            /*
+            for(int i=0;i<a.length;i++)
+				for (int j=0;j<a[i].length;j++)
+					for (int k=0;k<directions;k++)
+						searchTree(goDirections(a,i,j,k),l2);							
+			*/	
+            System.out.println("Counter"+counter);
+            end = System.currentTimeMillis( );
+            System.out.println( "For tress, Elapsed time in ms: " + (end-start) );
 
 			
 // Search from Hash table
-start = System.currentTimeMillis( );
-for(int i=0;i<rows;i++)
-	for (int j=0;j<columns;j++)
-	{
-		String str=String.valueOf(a[i][j]);
-		searchQuadraticProbing(str,l3);
-		//Go right
-		
-		for (int y=j+1; y<columns ;y++)
-		{
-			str+=String.valueOf(a[i][y]);
-			searchQuadraticProbing(str,l3);
-		}
-			
-		//Go left
-		str=String.valueOf(a[i][j]);
-		for (int y=j-1; y>=0 ;y--)
-		{
-			str+=String.valueOf(a[i][y]);
-			searchQuadraticProbing(str,l3);
-		}
-			
-		//Go down
-		str=String.valueOf(a[i][j]);
-		for (int x=i+1; x<rows ;x++)
-		{
-			str+=String.valueOf(a[x][j]);
-			searchQuadraticProbing(str,l3);
-		}	
-			
-		//Go up
-		str=String.valueOf(a[i][j]);
-		for (int x=i-1; x>=0 ;x--)
-		{
-			str+=String.valueOf(a[x][j]);
-			searchQuadraticProbing(str,l3);
-		}	
-			
-		//Go SE	
-		str=String.valueOf(a[i][j]);
-		for (int x=i+1,y=j+1; x<rows && y<columns ;x++,y++)
-		{
-			str+=String.valueOf(a[x][y]);
-			searchQuadraticProbing(str,l3);
-		}	
-			
-		//Go SW
-		str=String.valueOf(a[i][j]);
-		for (int x=i+1,y=j-1; x<rows && y>=0 ;x++,y--)
-		{
-			str+=String.valueOf(a[x][y]);
-			searchQuadraticProbing(str,l3);
-		}
-		//Go NW
-		str=String.valueOf(a[i][j]);
-		for (int x=i-1,y=j-1; x>=0 && y>=0 ;x--,y--)
-		{
-			str+=String.valueOf(a[x][y]);
-			searchQuadraticProbing(str,l3);
-		}
-		
-		//Go NE
-		str=String.valueOf(a[i][j]);
-		for (int x=i-1,y=j+1; x>=0 && y<columns ;x--,y++)
-		{
-			str+=String.valueOf(a[x][y]);
-			searchQuadraticProbing(str,l3);
-		}			
-
-	}
-System.out.println("Counter"+counter);
-end = System.currentTimeMillis( );
-System.out.println( "For cuckoo hash table, Elapsed time in ms: " + (end-start) );
+            start = System.currentTimeMillis( );
+            counter=0;
+            for(int i=0;i<a.length;i++)
+				for (int j=0;j<a[i].length;j++)
+					for (int k=0;k<directions;k++)
+						searchQuadraticProbing(goDirections(a,i,j,k),l3);
+										
+            System.out.println("Counter"+counter);
+            end = System.currentTimeMillis( );
+            System.out.println( "For hash table, Elapsed time in ms: " + (end-start) );
 			
 			
 		} 
@@ -285,7 +99,6 @@ System.out.println( "For cuckoo hash table, Elapsed time in ms: " + (end-start) 
 		
 	public static void searchLinkedList(String str,LinkedList<String> l)
 	{	
-		
 		//System.out.println("Str:"+str);
 		boolean flag=false;
 		Iterator<String> itr=l.iterator();
@@ -320,11 +133,64 @@ System.out.println( "For cuckoo hash table, Elapsed time in ms: " + (end-start) 
 	
 	public static void displayCrossword(char a[][])
 	{
-		for(int i=0;i<rows;i++)
+		for(int i=0;i<a.length;i++)
 		{
-			for (int j=0;j<columns;j++)
+			for (int j=0;j<a[i].length;j++)
 				System.out.print(a[i][j]+" ");
 			System.out.println("");
 		}	
+	}
+	
+	
+	public static String goDirections(char a[][],int i,int j,int k)
+	{
+		
+		StringBuilder sb = new StringBuilder();
+		//System.out.println("k:"+k);
+		switch (k)
+		{
+			case 0:
+				sb.setLength(0);
+				for (int y=j; y<a[i].length ;y++)
+					sb.append(a[i][y]);
+				break;
+			case 1:
+				sb.setLength(0);
+				for (int x=i,y=j; x<a.length && y<a[i].length ;x++,y++)
+					sb.append(a[x][y]);
+				break;
+			case 2:
+				sb.setLength(0);
+				for (int x=i; x<a.length ;x++)
+					sb.append(a[x][j]);
+				break;
+			case 3:
+				sb.setLength(0);
+				for (int x=i,y=j; x<a.length && y>=0 ;x++,y--)
+					sb.append(a[x][y]);
+				break;
+			case 4:
+				sb.setLength(0);
+				for (int y=j; y>=0 ;y--)
+					sb.append(a[i][y]);
+				break;
+			case 5:
+				sb.setLength(0);
+				for (int x=i,y=j; x>=0 && y>=0 ;x--,y--)
+					sb.append(a[i][y]);
+				break;
+			case 6:	
+				sb.setLength(0);
+				for (int x=i; x>=0 ;x--)
+					sb.append(a[x][j]);
+				break;
+			case 7:
+				sb.setLength(0);
+				for (int x=i,y=j; x>=0 && y<a[i].length ;x--,y++)
+					sb.append(a[i][y]);
+				break;
+		}
+		
+		return new String(sb);
 	}
 }
